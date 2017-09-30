83a3e4c8535a29365da6ba373c8ca5a420ae5128
Paul Craig <paul_craig_16@hotmail.com>
2017-09-30 23:04:52 +0100

    Add gulp responsive and picturefill

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const gulp = require('gulp')
 const postcss = require('gulp-postcss')
 const standard = require('gulp-standard')
+const $ = require('gulp-load-plugins')();
 
 function processCSS (gulp, filenames, fn) {
   filenames.forEach((filename) => {
@@ -50,10 +51,40 @@ gulp.task('jslint', function () {
     }))
 })
 
+gulp.task('img', function () {
+  return gulp.src('i/src/*.{jpg,png}')
+    .pipe($.responsive({
+      // Convert all images to JPEG format
+      '*': [{
+        // image-sm.jpg is 480 pixels wide
+        width: 480,
+        rename: {
+          suffix: '-sm',
+          extname: '.jpg',
+        },
+      }, {
+        // image-md.jpg is 768 pixels wide
+        width: 768,
+        rename: {
+          suffix: '-md',
+          extname: '.jpg',
+        },
+      }, {
+        // image-lg.jpg is 768 pixels wide
+        width: 1240,
+        rename: {
+          suffix: '-lg',
+          extname: '.jpg',
+        },
+      }],
+    }))
+    .pipe(gulp.dest('i/dst'));
+});
+
 gulp.task('css', ['csslint', 'autoprefix'])
 
 gulp.task('lint', ['csslint', 'jslint'])
 
-gulp.task('all', ['csslint', 'autoprefix', 'jslint'])
+gulp.task('all', ['csslint', 'autoprefix', 'jslint', 'img'])
 
-gulp.task('default', ['autoprefix'])
+gulp.task('default', ['autoprefix', 'img'])
